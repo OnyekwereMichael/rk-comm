df349ad647da4bed0cb1b1737a0d8976d6de7d78
OnyekwereMichael <onyekweremichael55@gmail.com>
2023-12-08 07:38:31 -0800

    first commit

--- src/PAGES/Header.js
+++ src/PAGES/Header.js
@@ -385,11 +385,11 @@ function Header() {
    
    <section className='flex justify-center mt-7 '>
    <div>
-       <input type="text" placeholder='write your name' className='rounded pl-2 pr-2 p-1'/>
+       <input type="text" placeholder='Your email address' className='rounded pl-2 pr-2 p-1'/>
    </div>
 
    <div>
-       <button className='inp rounded pl-2 pr-2 p-1'>read more</button>
+       <button className='inp rounded pl-2 pr-2 p-1 bg-black-700 text-white'>Subscribe</button>
    </div>
    </section>
 
@@ -397,7 +397,7 @@ function Header() {
 <section className='grid grid-cols-5 gap-10 justify-between p-24 gdg'>
    <div>
           <ul>
-              <li className='font-semibold miles pb-2'>About</li>
+              <li className='font-semibold  pb-2'>About</li>
               <li>Rent on Rk</li>
               <li>Policies</li>
               <li>Help</li>
@@ -406,7 +406,7 @@ function Header() {
  
     <div>
         <ul>
-            <li className='font-semibold miles pb-2'>Socail</li>
+            <li className='font-semibold  pb-2'>Socail</li>
             <li>Facebook</li>
             <li>Twitter</li>
             <li>Instagram</li>
@@ -415,7 +415,7 @@ function Header() {
 
     <div>
           <ul>
-                <li className='font-semibold miles pb-2'>More</li>
+                <li className='font-semibold  pb-2'>More</li>
                 <li>Sitemap</li>
                 <li>Blog</li>
                 <li>Blog</li>
@@ -425,7 +425,7 @@ function Header() {
 
     <div>
           <ul>
-              <li className='font-semibold miles pb-2'>About</li>
+              <li className='font-semibold  pb-2'>About</li>
               <li>Rent on Rk</li>
               <li>Policies</li>
               <li>Help</li>
@@ -434,12 +434,23 @@ function Header() {
 
     <div>
             <ul>
-                  <li className='font-semibold miles pb-2'>Contact</li>
+                  <li className='font-semibold  pb-2'>Contact</li>
                   <li>497, Evergreen road, Roseville CA 98</li>
                   <li>+44 398 234 7863</li>
                   <li>hello@rkcommuni</li>
           </ul>
     </div>
+    </section>
+    <hr/>
+
+    <section className='flex'>
+            <div>
+                Term of us
+            </div>
+
+            <div>
+                 Policies
+            </div>
     </section>
                 </footer>
         </motion.header>
